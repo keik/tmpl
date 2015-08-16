191f2b8731b4b78a4e407d7a709019a829504b7b
keik <k4t0.kei@gmail.com>
2015-08-16 23:35:30 +0900

    Add deps

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+    through = require('through2');
+
+gulp.task('html-minifier', function() {
+  // TODO
+});
+
+gulp.task('clean-css', function() {
+  // TODO
+});
+
+gulp.task('uglifyjs', function() {
+  // TODO
+});
